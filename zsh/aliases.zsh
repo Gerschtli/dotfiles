28f242a767aa61323cedf655abb98aa8a5889918
Tobias Happ <tobias.happ@gmx.de>
2016-08-28 16:44:49 +0200

    Added coloring for grep

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,10 @@
 alias zsh-reload="source ${HOME}/.zshrc"
 
-alias ls="ls --color=tty"
+alias ls="ls --color=auto"
 alias la="ls -AFv"
 alias ll="ls -AFhlv"
+
+alias grep="grep --color=auto"
 
 available xdg-open && alias open="xdg-open"
 
