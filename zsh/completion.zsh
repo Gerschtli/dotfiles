2ccf0f350f0bde9d48f4e64192de02b6c53f2e07
Tobias Happ <tobias.happ@gmx.de>
2016-08-28 04:19:24 +0200

    Updated general completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,2 +1,8 @@
 # hyphen-insensitive completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# add list-colors
+zstyle ':completion:*' list-colors ''
+
+# highlight selected option
+zstyle ':completion:*:*:*:*:*' menu select
