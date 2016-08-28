3abe462fa3ae42a67c98d1b2d5f72da18c62427f
Tobias Happ <tobias.happ@gmx.de>
2016-08-28 02:38:17 +0200

    Added language settings

---
+++ zsh/exports.zsh
@@ -0,0 +1,18 @@
+export TZ="Europe/Berlin"
+
+export LC_CTYPE=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=de_DE.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_PAPER=de_DE.UTF-8
+export LC_NAME=en_US.UTF-8
+export LC_ADDRESS=en_US.UTF-8
+export LC_TELEPHONE=en_US.UTF-8
+export LC_MEASUREMENT=de_DE.UTF-8
+export LC_IDENTIFICATION=en_US.UTF-8
+export LC_ALL=
+
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en:de_DE:de
