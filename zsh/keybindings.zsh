9db30ecbb775327361b7a4de9cd636959b0871b1
Tobias Happ <tobias.happ@gmx.de>
2016-08-31 22:19:54 +0200

    Added keybinding for delete-char

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,3 +1,7 @@
 [[ -n "${key[Up]}"     ]] && bindkey "${key[Up]}"     history-search-backward
 [[ -n "${key[Down]}"   ]] && bindkey "${key[Down]}"   history-search-forward
 [[ -n "${key[Delete]}" ]] && bindkey "${key[Delete]}" delete-char
+
+if [[ "${terminfo[kdch1]}" != "" ]]; then
+    bindkey "${terminfo[kdch1]}" delete-char
+fi
