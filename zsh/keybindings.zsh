2d7629cbb7a80733d1e828d9430f691969644fbc
Tobias Happ <tobias.happ@gmx.de>
2016-08-31 11:56:49 +0200

    Changed bahaviour of delete key

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,2 +1,3 @@
-[[ -n "${key[Up]}"   ]] && bindkey "${key[Up]}"   history-search-backward
-[[ -n "${key[Down]}" ]] && bindkey "${key[Down]}" history-search-forward
+[[ -n "${key[Up]}"     ]] && bindkey "${key[Up]}"     history-search-backward
+[[ -n "${key[Down]}"   ]] && bindkey "${key[Down]}"   history-search-forward
+[[ -n "${key[Delete]}" ]] && bindkey "${key[Delete]}" delete-char
