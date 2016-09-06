8b4c5bec1046fb3418bd6009ed703d7749d5fc83
Tobias Happ <tobias.happ@gmx.de>
2016-09-06 16:20:50 +0200

    Changed aliases for composer

--- php/aliases.zsh
+++ php/aliases.zsh
@@ -1,8 +1,8 @@
 if available php; then
-    alias com='bin/composer.phar'
-    alias com-install='com --prefer-source install'
-    alias com-update='com --prefer-source update'
-    alias com-download='mkdir -p bin; curl -sS https://getcomposer.org/installer | php -- --install-dir=bin'
+    alias composer='./bin/composer.phar'
+    alias cinstall='composer --prefer-source install'
+    alias cupdate='composer --prefer-source update'
+    alias cdownload='mkdir -p bin; curl -sS https://getcomposer.org/installer | php -- --install-dir=bin'
 
     alias ut='./bin/phpunit'
 fi
