c3c41193ebf56f6e62965eabe4632dd1b5f44b85
Tobias Happ <tobias.happ@gmx.de>
2016-09-08 01:29:19 +0200

    Cleaned functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,13 +1,12 @@
 available() {
-    return $(which "${1}" > /dev/null 2>&1)
+    which "${1}" > /dev/null 2>&1
 }
 
 is_ssh() {
     p=${1:-$PPID}
-    # read pid name x ppid y < <( cat /proc/$p/stat )
     read pid name ppid < <(ps -o pid= -o comm= -o ppid= -p $p)
     [[ "$name" =~ sshd ]] && return 0
-    [ "$ppid" -le 1 ]     && return 1
+    [[ "$ppid" -le 1 ]]   && return 1
     is_ssh $ppid
 }
 
