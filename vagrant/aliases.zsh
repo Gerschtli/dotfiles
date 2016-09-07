c5db01915f79fccb59ec92ff8bccd38522da56b3
Tobias Happ <tobias.happ@gmx.de>
2016-09-07 16:42:52 +0200

    Added aliases

--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -3,6 +3,7 @@ if available vagrant; then
     alias vdes="vagrant destroy"
     alias vhalt="vagrant halt"
     alias vpro="vagrant provision"
+    alias vrel="vagrant reload"
     alias vssh="vagrant ssh"
     alias vst="vagrant status"
     alias vup="vagrant up"
