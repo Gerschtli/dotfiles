e49141270bf8a98154fcaa88b3340430e17f1fd3
Tobias Happ <tobias.happ@gmx.de>
2016-09-16 01:07:12 +0200

    Removed is_ssh function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,14 +2,6 @@ available() {
     which "${1}" > /dev/null 2>&1
 }
 
-is_ssh() {
-    p=${1:-$PPID}
-    read pid name ppid < <(ps -o pid= -o comm= -o ppid= -p $p)
-    [[ "$name" =~ sshd ]] && return 0
-    [[ "$ppid" -le 1 ]]   && return 1
-    is_ssh $ppid
-}
-
 is_osx() {
     [[ "$(uname -s)" == "Darwin" ]]
 }
