d1cb3bf5ed79dd435b391fd3b8a5c22ce1b1de35
Tobias Happ <tobias.happ@gmx.de>
2016-10-10 12:22:54 +0200

    Fixed path setting

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,10 +1,10 @@
 HOME_BIN="${HOME}/bin"
 DOTFILES_BIN="${DOTFILES_ROOT}/bin"
 
-if [[ -d "${HOME}/bin" && "${PATH}" != *:"${HOME_BIN}":* ]]; then
+if [[ -d "${HOME}/bin" && "${PATH}" != "${HOME_BIN}":* ]]; then
     export PATH="${HOME}/bin:${PATH}"
 fi
-if [[ "${PATH}" != *:"${DOTFILES_BIN}":* ]]; then
+if [[ "${PATH}" != "${DOTFILES_BIN}":* ]]; then
     export PATH="${DOTFILES_ROOT}/bin:${PATH}"
 fi
 
