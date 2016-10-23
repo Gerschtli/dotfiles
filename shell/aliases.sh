1bd8d473afc91114ed42fe66ccdcf4704a4a0275
Tobias Happ <tobias.happ@gmx.de>
2016-10-23 05:31:39 +0200

    Added coloring for egrep and fgrep

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,6 +7,10 @@ fi
 alias la="ls -AFv"
 alias ll="ls -AFhlv"
 
-alias grep="grep --color=auto"
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 available xdg-open && alias open="xdg-open"
+
+available dircolors && eval "$(dircolors -b)"
