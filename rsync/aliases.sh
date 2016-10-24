1f4ae7263a25638d0f3faf8cbc8b8e66e7df795b
Tobias Happ <tobias.happ@gmx.de>
2016-10-24 09:19:15 +0200

    Updated rsync aliases for more generic purpose

--- rsync/aliases.sh
+++ rsync/aliases.sh
@@ -1,9 +1,25 @@
 if available rsync; then
     OPTIONS="-avru --delete --chmod=\"ug=rwX,o=rX\" --cvs-exclude --whole-file \
         --exclude-from ${DOTFILES_ROOT}/rsync/rsync-ignore"
-    alias rsa="rsync ${OPTIONS} ./ vagrant@admin.local.de:/var/www/htdocs"
-    alias rsb="rsync ${OPTIONS} ./ vagrant@backend.local.local:/var/www/htdocs"
-    alias rsf="rsync ${OPTIONS} ./ vagrant@preisvergleich.local.org:/var/www/htdocs"
-    alias rsp="rsync ${OPTIONS} ./ vagrant@profitmax.local.de:/var/www/htdocs"
+
+    alias rs="rsync ${OPTIONS}"
+
+    function rsc() {
+        local conf="rsync.conf"
+        if [[ -r "${conf}" ]]; then
+            rs "$(cat "${conf}")"
+        else
+            echo "${conf} not found, aborting..."
+        fi
+    }
+
+    alias rsa="echo -e 'deprecated, use\n\techo \". vagrant@admin.local.de:/var/www/htdocs\" > rsync.conf' \
+        && rs . vagrant@admin.local.de:/var/www/htdocs"
+    alias rsb="echo -e 'deprecated, use\n\techo \". vagrant@adminbackend.local.localde:/var/www/htdocs\" > rsync.conf' \
+        && rs . vagrant@backend.local.local:/var/www/htdocs"
+    alias rsf="echo -e 'deprecated, use\n\techo \". vagrant@adminpreisvergleich.local.orgde:/var/www/htdocs\" > rsync.conf' \
+        && rs . vagrant@preisvergleich.local.org:/var/www/htdocs"
+    alias rsp="echo -e 'deprecated, use\n\techo \". vagrant@profitmax.local.de:/var/www/htdocs\" > rsync.conf' \
+        && rs . vagrant@profitmax.local.de:/var/www/htdocs"
     unset OPTIONS
 fi
