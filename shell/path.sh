1a4a5b771b118ebb17998a718ec7b1a670ea6393
Tobias Happ <tobias.happ@gmx.de>
2016-10-26 00:41:40 +0200

    Introduced EXTENDED_PATH variable

--- shell/path.sh
+++ shell/path.sh
@@ -1,13 +1,7 @@
-DOTFILES_BIN="${DOTFILES_ROOT}/bin"
-
-for dir in "${HOME}"{/local,}/bin; do
+for dir in "${EXTENED_PATH[@]}"; do
     if [[ -d "${dir}" && "${PATH}" != "${dir}":* ]]; then
         export PATH="${dir}:${PATH}"
     fi
 done
 
-if [[ "${PATH}" != "${DOTFILES_BIN}":* ]]; then
-    export PATH="${DOTFILES_ROOT}/bin:${PATH}"
-fi
-
-unset dir DOTFILES_BIN
+unset dir
