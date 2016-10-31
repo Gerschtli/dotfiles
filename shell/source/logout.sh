3c23ce91109114f4a8e7ec9bdea9c87fcea40fd9
Tobias Happ <tobias.happ@gmx.de>
2016-10-31 01:15:16 +0100

    Remove .lesshst on logout

--- shell/source/logout.sh
+++ shell/source/logout.sh
@@ -1,3 +1,4 @@
+[[ -e "${HOME}/.lesshst" ]]             && rm -f "${HOME}/.lesshst"
 [[ -e "${HOME}/.xsession-errors.old" ]] && rm -f "${HOME}/.xsession-errors.old"
 
 [[ -x "/usr/bin/clear_console" ]] && clear_console -q
