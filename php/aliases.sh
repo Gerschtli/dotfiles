1cbe7234c3d63d5585ba389360769517d2b5ce17
Tobias Happ <tobias.happ@gmx.de>
2016-11-18 16:08:13 +0100

    Added optimize-autoloader option

--- php/aliases.sh
+++ php/aliases.sh
@@ -1,7 +1,7 @@
 if available php; then
     alias composer='./bin/composer.phar'
-    alias cinstall='composer --prefer-source install'
-    alias cupdate='composer --prefer-source update'
+    alias cinstall='composer --prefer-source --optimize-autoloader install'
+    alias cupdate='composer --prefer-source --optimize-autoloader update'
     alias csupdate='composer self-update'
     alias cdownload='mkdir -p bin && curl -sS "https://getcomposer.org/installer" | php -- --install-dir=bin'
 
