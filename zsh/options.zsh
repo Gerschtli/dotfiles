15d0fb9ced45f5a0008f57a1c6daa334ac65dbf0
Tobias Happ <tobias.happ@gmx.de>
2016-12-14 12:15:20 +0100

    Added zsh options

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,5 +1,11 @@
-setopt histignorealldups sharehistory
-
 HISTFILE="${HOME}/.zsh_history"
 HISTSIZE=10000
 SAVEHIST=10000
+
+setopt histignorealldups
+setopt sharehistory
+
+setopt auto_pushd
+setopt pushd_ignore_dups
+
+setopt append_history
