354515f4162896ad460d25e37540f94cee537a7c
Tobias Happ <tobias.happ@gmx.de>
2016-12-15 16:27:28 +0100

    Fixed keybindings

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,5 +1,10 @@
-[[ -n "${key[Up]}"     ]] && bindkey "${key[Up]}"     history-search-backward
-[[ -n "${key[Down]}"   ]] && bindkey "${key[Down]}"   history-search-forward
+autoload -Uz zkbd
+
+bindkey '\e[A' up-line-or-search
+bindkey '\e[B' down-line-or-search
+
+[[ -n "${key[Up]}"     ]] && bindkey "${key[Up]}"     up-line-or-search
+[[ -n "${key[Down]}"   ]] && bindkey "${key[Down]}"   down-line-or-search
 [[ -n "${key[Delete]}" ]] && bindkey "${key[Delete]}" delete-char
 
 if [[ "${terminfo[kdch1]}" != "" ]]; then
