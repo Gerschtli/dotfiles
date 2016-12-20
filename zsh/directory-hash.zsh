74a2106236f655ba2425f0a0bca007020e89aa9a
Tobias Happ <tobias.happ@gmx.de>
2016-12-20 01:20:33 +0100

    Added descriptive comment in directory-hash.zsh

--- zsh/directory-hash.zsh
+++ zsh/directory-hash.zsh
@@ -14,6 +14,7 @@ DROPBOX="${HOME}/Dropbox/Studium"
 
 if [[ -d "${DROPBOX}" ]]; then
     for i in "${DROPBOX}"/*(/); do
+        # to lowercase, replace spaces with dashes and remove plus symbols
         hash -d "uni-${${${$(basename ${i}):l}:gs/ /-/}:gs/+//}"="${i}"
     done
 fi
