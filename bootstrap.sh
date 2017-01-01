25d1b4e3037aea3559b19833ed8d097a76b82e6e
Tobias Happ <tobias.happ@gmx.de>
2017-01-01 04:27:04 +0100

    Added highlighting of filenames in bootstrap.sh and increased readability

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,17 +22,16 @@ _d_link_file() {
         return
     fi
 
-    echo "${src}:${dst}" >> "${CACHE_FILE}"
-
-    if [[ -e  "${dst}" && ! -L "${dst}" ]]; then
+    if [[ -e "${dst}" && ! -L "${dst}" ]]; then
         mv "${dst}" "${dst}.backup"
-        _d_info "Moved $dst to ${dst}.backup"
+        _d_info "Moved %s to %s" "${dst}" "${dst}.backup"
     fi
 
     if $(ln -snf "${src}" "${dst}"); then
-        _d_success "Linked ${src} to ${dst}"
+        _d_success "Linked %s to %s" "${src}" "${dst}"
+        echo "${src}:${dst}" >> "${CACHE_FILE}"
     else
-        _d_error "Failed linking ${src} to ${dst}"
+        _d_error "Failed linking %s to %s" "${src}" "${dst}"
         FAIL=true
     fi
 }
@@ -69,28 +68,28 @@ for old_link in "${links[@]}"; do
     if [[ -L "${old_link}" ]]; then
         if $(rm "${old_link}"); then
             _d_remove_line_in_cache "${old_link}"
-            _d_success "Removed link ${old_link}"
+            _d_success "Removed link %s" "${old_link}"
 
             if [[ -e "${old_link}.backup" ]]; then
                 if $(mv "${old_link}.backup" "${old_link}"); then
-                    _d_success "Restored backup ${old_link}.backup"
+                    _d_success "Restored backup %s" "${old_link}.backup"
                 else
-                    _d_error "Backup could not be moved: ${old_link}.backup"
+                    _d_error "Backup could not be moved: %s" "${old_link}.backup"
                     FAIL=true
                 fi
             fi
         else
-            _d_error "Link could not be deleted: ${old_link}"
+            _d_error "Link could not be deleted: %s" "${old_link}"
             FAIL=true
         fi
     else
         _d_remove_line_in_cache "${old_link}"
-        _d_info "${old_link} is not a link, updating cache"
+        _d_info "%s is not a link, updating cache" "${old_link}"
     fi
 done
 
 if [[ ! -z "${FAIL}" ]]; then
-    _d_error "-> An error occured during linking process. Please consider the log above."
+    _d_error "=> An error occured during linking process. Please consider the log above."
     exit 1
 fi
 
