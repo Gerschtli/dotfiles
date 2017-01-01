e0e11ef35e446c10b1e11cae72bcc475219b615a
Tobias Happ <tobias.happ@gmx.de>
2017-01-01 21:01:55 +0100

    Updated cdownload

--- php/aliases.sh
+++ php/aliases.sh
@@ -1,11 +1,25 @@
 if available php; then
-    alias composer='./bin/composer.phar'
+    alias composer='./bin/composer'
     alias cinstall='composer --prefer-source --optimize-autoloader install'
     alias cupdate='composer --prefer-source --optimize-autoloader update'
     alias csupdate='composer self-update'
-    alias cdownload='mkdir -p bin && curl -sS "https://getcomposer.org/installer" | php -- --install-dir=bin'
-
     alias ut='./bin/phpunit'
+
+    cdownload() {
+        if [ ! -e "bin/composer" ]; then
+            php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+            local EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+            local ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+            if [[ "${EXPECTED_SIGNATURE}" == "${ACTUAL_SIGNATURE}" ]]; then
+                mkdir -p bin
+                php ./composer-setup.php --install-dir=bin --filename=composer
+            fi
+
+            rm -f ./composer-setup.php
+        fi
+    }
 
     if available phpenmod && available phpdismod; then
         alias phpenxdebug='sudo phpenmod -s cli xdebug'
