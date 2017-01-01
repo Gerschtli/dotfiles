8dea78473faa272aa8d7122dc65e3067ea0119e1
Tobias Happ <tobias.happ@gmx.de>
2017-01-01 20:50:34 +0100

    Updated rsc function

--- rsync/functions.sh
+++ rsync/functions.sh
@@ -1,14 +1,13 @@
 if available rsync; then
     rsc() {
-        # content of conf:
-        # <local-dir> <host>:<remote-dir>
-
         local conf="rsync.conf"
         if [[ -r "${conf}" ]]; then
             rsync -avru --delete --chmod="ug=rwX,o=rX" --cvs-exclude --whole-file \
                 --exclude-from "${DOTFILES_ROOT}/rsync/rsync-ignore" $(cat "${conf}")
         else
             echo "${conf} not found, aborting..."
+            echo "\tcontent of ${conf}:"
+            echo "\t<local-dir> <user>@<host>:<remote-dir>"
         fi
     }
 fi
