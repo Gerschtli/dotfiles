c62a06a6c92d72d63240fba58557e1755a5fc8e7
Tobias Happ <tobias.happ@gmx.de>
2017-01-13 14:14:09 +0100

    Fixed ant git hooks scripts

--- git/templates/hooks/scripts/ant.sh
+++ git/templates/hooks/scripts/ant.sh
@@ -2,8 +2,8 @@
 
 BUILD_FILE="${PWD}"/build.xml
 
-if hash ant > /dev/null 2>&1 &&
-    [[ -e "${BUILD_FILE}" && $(grep '<target name="check"' "${BUILD_FILE}" | wc -l) != 0 ]]; then
+if hash ant > /dev/null 2>&1 && [ -e "${BUILD_FILE}" ] &&
+    [ $(grep '<target name="check"' "${BUILD_FILE}" | wc -l) != 0 ]; then
     ant check; RESULT=$?
 else
     RESULT=0
