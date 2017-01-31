2df3ca79a570042f4f0052461550402c7b3b71b6
Tobias Happ <tobias.happ@gmx.de>
2017-01-31 22:00:10 +0100

    Fixed bash startup files

--- bash/functions.bash
+++ bash/functions.bash
@@ -1,5 +1,3 @@
 shell-reload() {
-    [[ -r "${HOME}/.profile" ]]      && source "${HOME}/.profile"
     [[ -r "${HOME}/.bash_profile" ]] && source "${HOME}/.bash_profile"
-    [[ -r "${HOME}/.bashrc" ]]       && source "${HOME}/.bashrc"
 }
