971235ecae68f9f301d12ba4905031c8b64edb34
Tobias Happ <tobias.happ@gmx.de>
2017-02-08 23:26:24 +0100

    Execute npm install on post-checkout

--- git/templates/hooks/scripts/npm.sh
+++ git/templates/hooks/scripts/npm.sh
@@ -2,11 +2,23 @@
 
 PACKAGE_JSON="${PWD}"/package.json
 
-if hash npm > /dev/null 2>&1 && [ -e "${PACKAGE_JSON}" ] &&
-    [ $(npm run | grep "  test" | wc -l) != 0 ]; then
-    npm test; RESULT=$?
-else
-    RESULT=0
+RESULT=0
+
+npm_test() {
+    if [ $(npm run | grep "  test" | wc -l) != 0 ]; then
+        npm test; RESULT=$?
+    fi
+}
+
+if hash npm > /dev/null 2>&1 && [ -e "${PACKAGE_JSON}" ]; then
+    case "${1}" in
+        post-checkout | post-merge)
+            npm install
+            ;;
+        pre-push)
+            npm_test
+            ;;
+    esac
 fi
 
 exit ${RESULT}
