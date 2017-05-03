09a768cf8f7cdd0cb15c360bf652f4efba9b661d
Tobias Happ <tobias.happ@gmx.de>
2017-05-03 19:09:29 +0200

    Add vimplugupdate alias

--- nvim/aliases.sh
+++ nvim/aliases.sh
@@ -1,1 +1,9 @@
-available nvim && alias nvplugupdate="nvim +PlugUpgrade +PlugInstall +PlugUpdate +qall"
+if available vim && _d_has_option "nvim:enable-vim"; then
+    # vim config
+    alias vimplugupdate="vim +PlugUpgrade +PlugInstall +PlugUpdate +qall"
+fi
+
+if available nvim && ! _d_has_option "nvim:disable-nvim"; then
+    # nvim config
+    alias nvplugupdate="nvim +PlugUpgrade +PlugInstall +PlugUpdate +qall"
+fi
