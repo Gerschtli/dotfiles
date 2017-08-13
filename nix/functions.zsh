a9863ce3ee6fbf81f1d54ffffc5a81c75acb06c5
Tobias Happ <tobias.happ@gmx.de>
2017-08-14 00:27:44 +0200

    Append all parameter to nix-shell

--- nix/functions.zsh
+++ nix/functions.zsh
@@ -1,6 +1,6 @@
 if available nix-shell; then
     nshell() {
         local profile="${1}"
-        nix-shell "${DOTFILES_ROOT}/nix/profiles/${profile}.nix" --command zsh
+        nix-shell "${DOTFILES_ROOT}/nix/profiles/${profile}.nix" --command zsh "${@:2}"
     }
 fi
