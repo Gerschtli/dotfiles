15bc171c10a06d59e230d2d201e6b1f0d0837167
Tobias Happ <tobias.happ@gmx.de>
2017-08-14 22:41:44 +0200

    Adds nix-profile/share in XDG_DATA_DIRS

--- nix/env.sh
+++ nix/env.sh
@@ -1,3 +1,6 @@
 if _d_has_option "nix:source-nix-sh"; then
     source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
 fi
+
+# used for .desktop files
+export XDG_DATA_DIRS="${HOME}/.nix-profile/share:${XDG_DATA_DIRS}"
