2c12e581e00680ecb860e8e2b6c5990cc339d025
Tobias Happ <tobias.happ@gmx.de>
2017-08-16 14:22:57 +0200

    Fix java for osx

--- nix/profiles/modules/ant.nix
+++ nix/profiles/modules/ant.nix
@@ -1,3 +1,17 @@
-{ ant, oraclejdk8 }:
+{ ant, oraclejdk8, supportsJDK }:
 
-{ packages = [ ant oraclejdk8 ]; }
+let
+
+  # x86_64-darwin is not supported
+  packages = [
+    ant
+  ] ++ (if supportsJDK then [ oraclejdk8 ] else []);
+
+  environmentVariables =
+    if supportsJDK
+    then {}
+    else { JAVA_HOME = /Library/Java/JavaVirtualMachines/jdk1.8.0_71.jdk/Contents/Home; }; # /usr/libexec/java_home
+
+in
+
+{ inherit packages environmentVariables; }
