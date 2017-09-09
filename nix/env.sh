92c30276edd9075190caa775ad8645ce78e0bef8
Tobias Happ <tobias.happ@gmx.de>
2017-09-09 18:27:12 +0200

    Moves export of XDG_DATA_DIRS to xprofile

--- nix/env.sh
+++ nix/env.sh
@@ -1,6 +1,3 @@
 if _d_has_option "nix:source-nix-sh"; then
     source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
 fi
-
-# used for .desktop files
-export XDG_DATA_DIRS="${HOME}/.nix-profile/share:${XDG_DATA_DIRS}"
