a5b0e7772251a9484e30652a4933ad51027f1c87
Tobias Happ <tobias.happ@gmx.de>
2017-11-01 17:41:42 +0100

    Replaces oraclejdk8 with openjdk

--- nix/profiles/modules/ant.nix
+++ nix/profiles/modules/ant.nix
@@ -1,19 +1,8 @@
-# if supportsJDK is false, the jdk must be installed globally by the default package manager
+{ ant, jdk }:
 
-{ ant, oraclejdk8, supportsJDK }:
-
-let
-
-  # x86_64-darwin is not supported
+{
   packages = [
     ant
-  ] ++ (if supportsJDK then [ oraclejdk8 ] else []);
-
-  environmentVariables =
-    if supportsJDK
-    then {}
-    else { JAVA_HOME = /Library/Java/JavaVirtualMachines/jdk1.8.0_71.jdk/Contents/Home; }; # /usr/libexec/java_home
-
-in
-
-{ inherit packages environmentVariables; }
+    jdk
+  ];
+}
