1e7cb6e1d82b840ade7a4c99d1f52d68482ddd92
Tobias Happ <tobias.happ@gmx.de>
2017-11-26 13:39:04 +0100

    Adds plc tmux profiles

---
+++ tmux/profiles/plc-java.sh
@@ -0,0 +1,6 @@
+ROOT="${HOME}/projects/plc-web-java"
+TITLE="plc-java"
+
+PRESET="git-single"
+
+NIX_PROFILE="."
---
+++ tmux/profiles/plc-paper.sh
@@ -0,0 +1,6 @@
+ROOT="${HOME}/projects/plc-web-paper"
+TITLE="plc-paper"
+
+PRESET="git-single"
+
+NIX_PROFILE="."
