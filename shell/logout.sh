f8bacb6f45826c7a5407a03c3998ad2922c65037
Tobias Happ <tobias.happ@gmx.de>
2017-12-20 13:12:07 +0100

    Fixes shell/logout.sh

--- shell/logout.sh
+++ shell/logout.sh
@@ -1,5 +1,5 @@
 [[ -e "${HOME}/.lesshst" ]]             && rm -f "${HOME}/.lesshst"
 [[ -e "${HOME}/.xsession-errors.old" ]] && rm -f "${HOME}/.xsession-errors.old"
-[[ -e "${HOME}/.zcompdump*" ]]          && rm -f "${HOME}/.zcompdump*"
+[[ -e "${HOME}/.zcompdump" ]]           && rm -f "${HOME}/.zcompdump"*
 
-[[ -x "/usr/bin/clear_console" ]] && clear_console -q
+available clear && clear
