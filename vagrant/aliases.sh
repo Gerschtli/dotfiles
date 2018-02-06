8668cb2789ec43dbc1f5bf16ffb81bf796c9aaf2
Tobias Happ <tobias.happ@gmx.de>
2018-02-06 20:40:51 +0100

    Adds vagrant aliases

--- vagrant/aliases.sh
+++ vagrant/aliases.sh
@@ -1,12 +1,14 @@
 if available vagrant; then
     alias cdv="cd vagrant"
 
+    alias vauto="vagrant rsync-auto"
     alias vdes="vagrant destroy && rm -rf .vagrant"
     alias vhalt="vagrant halt"
     alias vpro="vagrant provision"
-    alias vrel="vagrant reload"
+    alias vrel="vagrant reload --provision"
     alias vssh="vagrant ssh"
     alias vst="vagrant status"
+    alias vsync="vagrant rsync"
     alias vup="vagrant up --provision"
 
     alias cdvpro="cdv && vpro && cd -"
