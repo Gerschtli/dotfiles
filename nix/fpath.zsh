46c39fe93e7fc14abaa6865a2f0f5bca48de93a6
Tobias Happ <tobias.happ@gmx.de>
2018-03-16 23:32:57 +0100

    Adds zsh completion for nix-shell buildInputs

--- nix/fpath.zsh
+++ nix/fpath.zsh
@@ -6,4 +6,19 @@ if [[ -d "${DIR}" ]]; then
     _d_register_fpath "${DIR}"
 fi
 
+# is set in nix.shell
+if [[ ! -z "${buildInputs}" ]]; then
+    for buildInput in "${(ps: :)buildInputs}"; do
+        DIR="${buildInput}/share/zsh/site-functions"
+        if [[ -d "${DIR}" ]]; then
+            _d_register_fpath "${DIR}"
+        fi
+
+        DIR="${buildInput}/share/zsh/${ZSH_VERSION}/functions"
+        if [[ -d "${DIR}" ]]; then
+            _d_register_fpath "${DIR}"
+        fi
+    done
+fi
+
 unset DIR
