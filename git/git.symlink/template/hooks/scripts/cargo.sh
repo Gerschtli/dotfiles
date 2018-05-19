eaf58dca84c43a678ea1b4949a0675d9e8e98108
Tobias Happ <tobias.happ@gmx.de>
2018-05-19 17:55:13 +0200

    Run cargo check before clippy in cargo  git hook
    Workaround because of bug in chrono or clippy.
    See https://github.com/chronotope/chrono/issues/246
    See https://github.com/rust-lang-nursery/rust-clippy/issues/2760

--- git/git.symlink/template/hooks/scripts/cargo.sh
+++ git/git.symlink/template/hooks/scripts/cargo.sh
@@ -5,7 +5,7 @@ source "${PWD}/.git/hooks/helpers/util.sh"
 CONFIG_FILE="${PWD}/Cargo.toml"
 
 check() {
-    cargo fmt -- --write-mode=diff; track_result
+    cargo fmt -- --check; track_result
     cargo build; track_result
     if $(cargo --list | grep clippy > /dev/null 2>&1); then
         cargo clippy; track_result
