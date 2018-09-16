425fb7ce01fba8ee3e4f7521ec60d8c79f3866b1
Tobias Happ <tobias.happ@gmx.de>
2018-09-16 09:39:22 +0200

    Adds tmux profiles for dfc projects

---
+++ tmux/profiles/dont-fall-crew-backend.sh
@@ -0,0 +1,5 @@
+ROOT="${HOME}/projects/dont-fall-crew-backend"
+
+LAYOUT="even-horizontal"
+
+PANE_CMDS=("git fm" "flask run")
---
+++ tmux/profiles/dont-fall-crew-frontend.sh
@@ -0,0 +1,5 @@
+ROOT="${HOME}/projects/dont-fall-crew-frontend"
+
+LAYOUT="even-horizontal"
+
+PANE_CMDS=("git fm" "npm run serve")
