ac38f0fa808078297aaa1e44e359effd8c3ade00
Tobias Happ <tobias.happ@gmx.de>
2018-10-01 15:37:24 +0200

    Enables php55 overlays only if php56 is available to override

--- nix/nixpkgs.symlink.config/overlays/php55.nix
+++ nix/nixpkgs.symlink.config/overlays/php55.nix
@@ -1,13 +1,17 @@
 self: super:
 
-{
-  php55 = super.php56.overrideDerivation (old: rec {
-    name = "php-${version}";
-    version = "5.5.38";
+if super ? "php56"
+then
+  {
+    php55 = super.php56.overrideDerivation (old: rec {
+      name = "php-${version}";
+      version = "5.5.38";
 
-    src = super.fetchurl {
-      url = "http://www.php.net/distributions/php-${version}.tar.bz2";
-      sha256 = "0f1y76whg6yx9a18mh97f8yq8lb64ri1f0zfr9la9374nbmq2g27";
-    };
-  });
-}
+      src = super.fetchurl {
+        url = "http://www.php.net/distributions/php-${version}.tar.bz2";
+        sha256 = "0f1y76whg6yx9a18mh97f8yq8lb64ri1f0zfr9la9374nbmq2g27";
+      };
+    });
+  }
+else
+  { }
--- nix/nixpkgs.symlink.config/overlays/php55Packages.nix
+++ nix/nixpkgs.symlink.config/overlays/php55Packages.nix
@@ -1,8 +1,12 @@
 self: super:
 
-{
-  php55Packages = (super.callPackage <nixpkgs/pkgs/top-level/php-packages.nix> {
-    pkgs = self;
-    php  = self.php55;
-  }) // { recurseForDerivations = true; };
-}
+if super ? "php55"
+then
+  {
+    php55Packages = (super.callPackage <nixpkgs/pkgs/top-level/php-packages.nix> {
+      pkgs = self;
+      php  = self.php55;
+    }) // { recurseForDerivations = true; };
+  }
+else
+  { }
