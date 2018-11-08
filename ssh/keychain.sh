2983dc5e3bc3338f010cb8f3ff3cc8e8959d06e0
Tobias Happ <tobias.happ@gmx.de>
2018-11-08 16:34:28 +0100

    Revert "Lazy init keychain"
    This reverts commit a64e72765131e8275e6a630b53fc5f4ff1174a1d.

--- ssh/keychain.sh
+++ ssh/keychain.sh
@@ -1,15 +1,12 @@
 if available keychain; then
+    eval $(keychain --agents ssh --eval --quiet)
+
     kadd() {
         local key="$(find "${DOTFILES_ROOT}/ssh/modules" -type f -name "id_rsa.${1}" | head -n 1)"
 
         if [[ ! -r "${key}" ]]; then
             echo "ssh key not found: ${key}"
         else
-            if [[ -z "${_D_KEYCHAIN_EVAL}" ]]; then
-                eval $(keychain --agents ssh --eval --quiet)
-                _D_KEYCHAIN_EVAL=1
-            fi
-
             keychain "${key}"
         fi
 
