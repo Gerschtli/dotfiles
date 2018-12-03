7742b9df1c4bb6bf2fb875bfcd945bccb69fe239
Tobias Happ <tobias.happ@gmx.de>
2018-12-03 11:40:32 +0100

    Add all-ubuntu env in all.nix overlay

--- nix/nixpkgs.symlink.config/overlays/all.nix
+++ nix/nixpkgs.symlink.config/overlays/all.nix
@@ -1,6 +1,22 @@
 self: super:
 
 {
+  all-ubuntu = super.buildEnv {
+    name = "all-ubuntu";
+
+    paths = with self; [
+      docker-compose
+      dwm-status
+      fzf
+      httpie
+      jq
+      playerctl
+      qpdfview
+      ripgrep
+      rxvt_unicode-with-plugins
+    ];
+  };
+
   all-wsl = super.buildEnv {
     name = "all-wsl";
 
