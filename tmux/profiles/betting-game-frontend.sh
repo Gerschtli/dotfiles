0c9efde982251200c82e83bb6e9a048d884ab0b9
Tobias Happ <tobias.happ@gmx.de>
2019-03-24 11:29:04 +0100

    Revert "Update betting-game-frontend tmux profile"
    This reverts commit d7a51682fcf3238533db285e8befbc9ef3d3b88b.

--- tmux/profiles/betting-game-frontend.sh
+++ tmux/profiles/betting-game-frontend.sh
@@ -2,4 +2,4 @@ ROOT="${HOME}/projects/betting-game-frontend"
 
 LAYOUT="even-horizontal"
 
-PANE_CMDS=("git fm" "nixops/manage dev start:nixops/manage dev ssh bgf:journalctl -xef")
+PANE_CMDS=("git fm" "npm run serve")
