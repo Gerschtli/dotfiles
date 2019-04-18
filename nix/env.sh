99a2ab891354ad05f5495a2c0100aae948262feb
Tobias Happ <tobias.happ@gmx.de>
2019-04-18 14:53:29 +0200

    Add fix unicode problem in urxvt on ubuntu
    See https://github.com/NixOS/nixpkgs/issues/38991#issuecomment-400657551

--- nix/env.sh
+++ nix/env.sh
@@ -6,4 +6,10 @@ if _d_has_option "nix:source-nix-sh"; then
     if is_zsh; then
         hash -f
     fi
+
+    # see: https://github.com/NixOS/nixpkgs/issues/38991#issuecomment-400657551
+    nix-build --no-out-link "<nixpkgs>" -A glibcLocales > /dev/null 2>&1
+
+    export LOCALE_ARCHIVE_2_27="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"
+    export LOCALE_ARCHIVE_2_11="/usr/bin/locale/locale-archive"
 fi
