c17575850b1a4fbba795eda6c71166ef793b5ba9
Tobias Happ <tobias.happ@gmx.de>
2019-06-04 16:06:13 +0200

    Add initial commands for pveu-test tmux profile

--- tmux/profiles/pveu-test.sh
+++ tmux/profiles/pveu-test.sh
@@ -1,3 +1,3 @@
 LAYOUT="even-horizontal"
 
-PANE_CMDS=("ssh pveu.test.backend" "ssh pveu.test.frontend")
+PANE_CMDS=("ssh pveu.test.backend:./install/list.sh" "ssh pveu.test.frontend:./install/list.sh")
