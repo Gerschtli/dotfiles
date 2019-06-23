2ce968a89521283aff3d6d028d803820ac0354fa
Tobias Happ <tobias.happ@gmx.de>
2019-06-23 14:55:40 +0200

    Remove rust-1-31 cargo feature in git hook

--- git/git.symlink/template/hooks/scripts/cargo.sh
+++ git/git.symlink/template/hooks/scripts/cargo.sh
@@ -6,10 +6,10 @@ CONFIG_FILE="${PWD}/Cargo.toml"
 
 check() {
     cargo fmt -- --check; track_result
-    cargo check --features "rust-1-31 mocking"; track_result
-    cargo build --features "rust-1-31 mocking"; track_result
-    cargo clippy --features "rust-1-31 mocking"; track_result
-    cargo test --features "rust-1-31 mocking"; track_result
+    cargo check --features "mocking"; track_result
+    cargo build --features "mocking"; track_result
+    cargo clippy --features "mocking"; track_result
+    cargo test --features "mocking"; track_result
 }
 
 if has_command_and_file cargo "${CONFIG_FILE}"; then
