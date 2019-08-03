340f8bcba252f7d03a9b14f6624f478dd442112f
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 14:40:33 +0200

    Remove ssh module

--- ssh/functions.sh
+++
@@ -1,7 +0,0 @@
-keygen() {
-    if [[ -z "${1}" ]]; then
-        echo "Enter path  as argument!"
-    else
-        ssh-keygen -t rsa -b 4096 -f "${1}"
-    fi
-}
--- ssh/profile.sh
+++
@@ -1,4 +0,0 @@
-# remove existing keys
-if [[ ${SHLVL} -eq 1 ]] && available keychain; then
-    keychain --clear --quiet
-fi
