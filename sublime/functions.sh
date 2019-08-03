043dbf1d1a35e73c0a2ff5f6f636a3b898dbc5ea
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 15:03:57 +0200

    Remove deprecated sublime module files

--- sublime/functions.sh
+++
@@ -1,8 +0,0 @@
-init-sftp() {
-    local host="${1}"
-    if [[ $# != 1 ]]; then
-        echo "provide host as argument"
-    else
-        sed "s/<host>/${host}/g" "${DOTFILES_ROOT}/sublime/User/sftp-config.json.dist" > sftp-config.json
-    fi
-}
