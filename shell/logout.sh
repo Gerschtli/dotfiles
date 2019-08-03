57bc9299de6bc472b6254c17ab91ff36612e5f14
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 14:40:46 +0200

    Remove shell module

--- shell/logout.sh
+++
@@ -1,5 +0,0 @@
-[[ -e "${HOME}/.lesshst" ]]             && rm -f "${HOME}/.lesshst"
-[[ -e "${HOME}/.xsession-errors.old" ]] && rm -f "${HOME}/.xsession-errors.old"
-[[ -e "${HOME}/.zcompdump" ]]           && rm -f "${HOME}/.zcompdump"*
-
-available clear && clear
