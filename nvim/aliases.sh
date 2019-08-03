1d8c954cffbf78cb9463febf78f8f76491a5b36f
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 14:41:50 +0200

    Remove nvim module

--- nvim/aliases.sh
+++
@@ -1,9 +0,0 @@
-if available vim && _d_has_option "nvim:enable-vim"; then
-    # vim config
-    alias vimplugupdate="vim +PlugUpgrade +PlugInstall +PlugUpdate +qall"
-fi
-
-if available nvim && ! _d_has_option "nvim:disable-nvim"; then
-    # nvim config
-    alias nvplugupdate="nvim +PlugUpgrade +PlugInstall +PlugUpdate +qall"
-fi
--- nvim/init.sh
+++
@@ -1,7 +0,0 @@
-PLUG_PATH="${DOTFILES_ROOT}/nvim/nvim/autoload/plug.vim"
-
-if [[ ! -r "${PLUG_PATH}" ]]; then
-    curl -Lso "${PLUG_PATH}" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-fi
-
-unset PLUG_PATH
