4a003e33919900bf90695b5380bd88284fafea9c
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 14:39:09 +0200

    Remove wm module

--- wm/util/systemctl-wrapper.sh
+++
@@ -1,8 +0,0 @@
-systemctl_wrapper() {
-    local action="${1}"
-    local string="${2}"
-
-    if zenity --question --text="Are you sure you want ${string}?" 2>/dev/null; then
-        echo $(systemctl $action)
-    fi
-}
